5fb7a55c0b8555e609137f0b68678cf8bbb03a9c
Fredrik Forstbach <fredrik.forstbach@gmail.com>
2017-04-18 16:59:35 +0200

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create(title: Faker::Book.title, content: Faker::StarWars.quote)
+Article.create(title: Faker::Book.title, content: Faker::StarWars.quote)
+Article.create(title: Faker::Book.title, content: Faker::StarWars.quote)
+Article.create(title: Faker::Book.title, content: Faker::StarWars.quote)
+Article.create(title: Faker::Book.title, content: Faker::StarWars.quote)
+Article.create(title: Faker::Book.title, content: Faker::StarWars.quote)
+Article.create(title: Faker::Book.title, content: Faker::StarWars.quote)
+Article.create(title: Faker::Book.title, content: Faker::StarWars.quote)
+Article.create(title: Faker::Book.title, content: Faker::StarWars.quote)
+Article.create(title: Faker::Book.title, content: Faker::StarWars.quote)
